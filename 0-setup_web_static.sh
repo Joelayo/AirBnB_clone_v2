0d52a0539574025789acbd0163274b76692e6289
Joelayo <joetech.ooj@gmail.com>
2022-09-29 23:31:06 +0100

    Deploying Web Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,9 +11,5 @@ echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 configure="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo chown -hR ubuntu:ubuntu /data/ 
-sudo sed -i "49i\ $configure" /etc/nginx/sites-available/default
-if [ "$(pgrep -c nginx)" -le 0 ]; then
-	service nginx start
-else
-	service nginx restart
-fi
+#sudo sed -i "49i\ $configure" /etc/nginx/sites-available/default
+sudo service nginx restart
