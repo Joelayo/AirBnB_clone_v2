275fc666d0505b6d1f093c313be5e21887d6c404
Joelayo <joetech.ooj@gmail.com>
2022-09-29 23:45:25 +0100

    Deploying Web Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,6 @@ sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo chown -R ubuntu /data/web_static/releases/test
 echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-configure="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo chown -hR ubuntu:ubuntu /data/ 
-sudo sed -i "49i\ $configure" /etc/nginx/sites-available/default
+sed -i -e '/location \/ /{:a; N; /\}/!ba; a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' -e '}' /etc/nginx/sites-available/default
 sudo service nginx restart
