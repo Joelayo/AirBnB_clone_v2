9795d9e6793d0f8852e29a6d08ca61cd16c3aad5
Joelayo <joetech.ooj@gmail.com>
2022-09-29 23:23:36 +0100

    Deploying Web Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # script to setup web server for the deployment of web_static
 sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
+if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	sudo apt-get -y install nginx;
+fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo chown -R ubuntu /data/web_static/releases/test
@@ -12,4 +14,3 @@ configure="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\
 sudo chown -hR ubuntu:ubuntu /data/ 
 sudo sed -i "49i\ $configure" /etc/nginx/sites-available/default
 sudo service nginx restart
-sudo service nginx reload
