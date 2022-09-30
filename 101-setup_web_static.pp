9778fa32f47587fdab8c60c090fd5fb6b25f18dc
Joelayo <joetech.ooj@gmail.com>
2022-09-30 05:55:37 +0100

    Deploying Web Static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,123 +1,78 @@
-#!/usr/bin/puppet apply
-# AirBnB clone web server setup and configuration
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
-  path    => '/usr/bin:/usr/sbin:/bin',
+# Manifest that configures a web server for deployment
+exec { 'exec_0':
+  command => 'sudo sudo apt-get update -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-exec { 'remove-current':
+exec { 'exec_1':
+  require => Exec['exec_0'],
+  command => 'sudo apt-get install nginx -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_2':
+  require => Exec['exec_1'],
+  command => 'sudo mkdir -p /data/web_static/shared/',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_3':
+  require => Exec['exec_2'],
+  command => 'sudo mkdir -p /data/web_static/releases/test/',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_4':
+  require => Exec['exec_3'],
+  command => 'sudo touch /data/web_static/releases/test/index.html',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_5':
+  require => Exec['exec_4'],
+  command => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html >/dev/null',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_6':
+  require => Exec['exec_5'],
   command => 'rm -rf /data/web_static/current',
-  path    => '/usr/bin:/usr/sbin:/bin',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
 
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+
+exec { 'exec_7':
+  require => Exec['exec_6'],
+  command => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
 
-file { '/var/www':
-  ensure  => directory,
-  mode    => '0755',
-  recurse => true,
-  require => Package['nginx'],
+exec { 'exec_8':
+  require => Exec['exec_7'],
+  command => 'sudo chown -R ubuntu:ubuntu /data/',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello, World!',
-  require => File['/var/www'],
+exec { 'exec_9':
+  require     => Exec['exec_8'],
+  environment => ['C=\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n'],
+  command     => 'sudo sed -i "38i $C" /etc/nginx/sites-available/default',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0,1]
 }
 
-file { '/var/www/error/404.html':
-  content => "Ceci n'est pas une page",
-  require => File['/var/www'],
-}
-
-exec { 'make-static-files-folder':
-  command => 'mkdir -p /data/web_static/releases/test /data/web_static/shared',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => Package['nginx'],
-}
-
-file { '/data/web_static/releases/test/index.html':
-  content =>
-"<!DOCTYPE html>
-<html lang='en-US'>
-	<head>
-		<title>Home - AirBnB Clone</title>
-	</head>
-	<body>
-		<h1>Welcome to AirBnB!</h1>
-	<body>
-</html>
-",
-  replace => true,
-  require => Exec['make-static-files-folder'],
-}
-
-exec { 'link-static-files':
-  command => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => [
-    Exec['remove-current'],
-    File['/data/web_static/releases/test/index.html'],
-  ],
-}
-
-exec { 'change-data-owner':
-  command => 'chown -hR ubuntu:ubuntu /data',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => Exec['link-static-files'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  mode    => '0644',
-  content =>
-"server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	server_name _;
-	index index.html index.htm;
-	error_page 404 /404.html;
-	add_header X-Served-By \$hostname;
-	location / {
-		root /var/www/html/;
-		try_files \$uri \$uri/ =404;
-	}
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-		try_files \$uri \$uri/ =404;
-	}
-	if (\$request_filename ~ redirect_me){
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
-	}
-	location = /404.html {
-		root /var/www/error/;
-		internal;
-	}
-}",
-  require => [
-    Package['nginx'],
-    File['/var/www/html/index.html'],
-    File['/var/www/error/404.html'],
-    Exec['change-data-owner']
-  ],
-}
-
-exec { 'enable-site':
-  command => "ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'",
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-exec { 'start-nginx':
+exec { 'exec_10':
+  require => Exec['exec_9'],
   command => 'sudo service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => [
-    Exec['enable-site'],
-    Package['nginx'],
-    File['/data/web_static/releases/test/index.html'],
-  ],
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
-
-Exec['start-nginx']
