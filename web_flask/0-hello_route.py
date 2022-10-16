fb583c85e51e0077e70cad79f347a4649773068e
Joelayo <joetech.ooj@gmail.com>
2022-10-16 21:40:59 +0100

    Flask web application script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-'''
-First program using Flask framework
+'''A simple Flask web application.
 '''
 from flask import Flask
 
 
 app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
-    '''Print a message from Flask'''
+def index():
+    '''The home page.'''
     return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
